6a65b7e73bacca5725d0f8f3c18d0b631e0d293a
Sebastian <sebastianllatasotoya15@gmail.com>
2025-11-14 15:32:28 -0500

    refactor: elimina java.js

--- java.js
+++
@@ -1,9 +0,0 @@
-try {
-  const response = await fetch('/api/tasks', {
-    method: 'POST',
-    body: JSON.stringify(task),
-  });
-  const data = await response.json();
-  setTasks([...tasks, data]);
-} catch (error) {
-  console.error('Error al crear la tarea:', error);
